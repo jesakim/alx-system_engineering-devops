7c0748ab6598664399df5d539a07ad55e5e269d7
Jamal Eddine Sakim <112765925+jesakim@users.noreply.github.com>
2023-11-24 19:31:15 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,5 @@
-# Install Flask using pip3
-exec { 'install flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-}
+# install puppet-lint -v 2.5.0
 
-# Notify the user about the installation
-notify { 'Flask installed successfully':
-  require => Exec['install flask'],
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
