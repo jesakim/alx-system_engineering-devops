899f535ece363636a841a67ba7d4c73c42230ebc
Jamal Eddine Sakim <112765925+jesakim@users.noreply.github.com>
2024-01-16 16:16:08 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
-"""Queries the Reddit API and
-returns the number of subscribers
-(not active users, total subscribers)
-for a given subreddit.
-
-If an invalid subreddit is given,
-the function should return 0.
 """
+module contains function to get no. of reddit's
+subreddit subscribers.
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the total number of subscribers
-    for a given subreddit.
     """
-    # Set the Default URL strings
-    base_url = 'https://www.reddit.com'
-    api_uri = '{base}/r/{subreddit}/about.json'.format(base=base_url,
-                                                       subreddit=subreddit)
+    query the reddit API, and fetch data about a
+    reddit's subreddit
+    """
 
-    # Set an User-Agent
-    user_agent = {'User-Agent': 'Python/requests'}
-
-    # Get the Response of the Reddit API
-    res = requests.get(api_uri, headers=user_agent,
-                       allow_redirects=False)
-
-    # Checks if the subreddit is invalid
-    if res.status_code in [302, 404]:
+    headers = {
+            "Accept": "*/*",
+            "User-Agent": "ALX student script",
+    }
+    api_url = 'https://www.reddit.com/r'
+    url = '{}/{}/about.json?raw_json=1'.format(api_url, subreddit)
+    res = requests.get(url, headers=headers)
+    if not res.ok:
         return 0
-
-    # Returns the total subscribers of the subreddit
-    return res.json().get('data', {}).get('subscribers', 0)
+    data = res.json().get("data")
+    subscribers = data.get("subscribers")
+    return subscribers
